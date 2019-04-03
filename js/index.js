abb36ba57ad2b053e96abec44fed9aeb95fb0e77
dlee16 <diana.lee116@gmail.com>
2019-04-03 17:20:47 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+const saveLincoln = document.getElementById("save_lincoln");
+let op = 1;
+const foreground = document.getElementById("foreground")
+
+function fade() {
+  op -= 0.1
+  foreground.style.opacity = op
 }
+
+saveLincoln.addEventListener("click", function(){
+  const interval = parseInt(document.querySelector("#interval").value);
+  setInterval(fade, interval)
+})
